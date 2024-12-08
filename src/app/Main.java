74517f6a04ae1d9925306a519c0d72956d98a9ba
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-08 03:44:29 +0200

    Add product №1 info

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,4 +8,16 @@ public class Main {
     private static final String CURRENCY = "EUR";
     public static double avgSales;
 
+    public static void main(String[] args) {
+        productNumber = 1;
+        product = "smartphone";
+        daysSales = 5;
+        totalSales = 12153.41;
+        avgSales = totalSales / daysSales;
+        System.out.printf("%nProduct № %d: %s," +
+                "%ntotal sales for %d days is %s %.2f," +
+                "%nsales by day is %s %.2f.%n",
+                productNumber, product, daysSales, CURRENCY,
+                totalSales, CURRENCY, avgSales);
+    }
 }
