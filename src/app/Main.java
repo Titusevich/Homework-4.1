5bc73f0ea2a4ab711e37b0cdc8bb07ee5376b368
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-08 02:57:55 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Main {
+    public static int productNumber;
+    public static String product;
+    public static int daysSales;
+    public static double totalSales;
+    private static final String CURRENCY = "EUR";
+    public static double avgSales;
+
+}
